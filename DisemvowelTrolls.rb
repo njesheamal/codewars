cafe970cf9aa3fc6946ce0b8a92ceb137a311ccf
Amal Abdu <abduamal@ymail.com>
2021-12-13 12:34:41 -0600

    initial solution to the challenge

--- DisemvowelTrolls.rb
+++ DisemvowelTrolls.rb
@@ -1,3 +1,13 @@
 # Your task is to write a function that takes a string and return a new string with all vowels removed.
 
 # For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+# SOLUTION:
+def disemvowel(str)
+  neutralized = str.split("").select do |char|
+    not "aeiouAEIOU".include?(char)
+  end
+
+  return neutralized.join
+end
+
